087981b5d6a6ef3926a67ccbb97312932bf704d5
Navaj Shaikh <mr.shaikh255256@gmail.com>
2026-02-18 09:24:16 +0530

    delete display fun

--- images/bag.js
+++ images/bag.js
@@ -2,6 +2,3 @@ function onLoad(){  // load the js file
     displayBagItems();   // display the bag items  
 }
 
-function displayBagItems(){
-    let containerElement = document.querySelector('.bag-items-container') 
-}
